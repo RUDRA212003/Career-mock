36b2fc4f723944f4a46b8da97e34a01477dbe822
Rudresh M <rudratiptur@gmail.com>
2025-12-24 23:58:16 +0530

    remove standalone output to fix vercel app router build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
+
   images: {
     remotePatterns: [
+      { protocol: "https", hostname: "img.freepik.com" },
+      { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "cdn-icons-png.flaticon.com" },
       {
-        protocol: 'https',
-        hostname: 'img.freepik.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn-icons-png.flaticon.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'grtduudyzchqzffvcbtz.supabase.co',
-        pathname: '/storage/v1/object/public/**',
+        protocol: "https",
+        hostname: "grtduudyzchqzffvcbtz.supabase.co",
+        pathname: "/storage/v1/object/public/**",
       },
     ],
   },
